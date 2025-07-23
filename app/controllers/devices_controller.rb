f4bf80088707c156c4fd288c59a3e34d157c0863
Jakub <35482785+Zimoslaw@users.noreply.github.com>
2025-07-23 22:20:59 +0200

    Updated controller

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -5,14 +5,31 @@ class DevicesController < ApplicationController
   def assign
     AssignDeviceToUser.new(
       requesting_user: @current_user,
-      serial_number: params[:serial_number],
-      new_device_owner_id: params[:new_device_owner_id]
+      serial_number: params.dig(:device, :serial_number),
+      new_device_owner_id: params[:new_owner_id]
     ).call
+
     head :ok
+  rescue RegistrationError::Unauthorized
+    render json: { error: 'Unauthorized' }, status: :unauthorized
+  rescue AssigningError::AlreadyUsedOnUser
+    render json: { error: 'This device was previously used by user' }, status: :ok
+  rescue AssigningError::AlreadyUsedOnOtherUser
+    render json: { error: 'This device is being used by another user' }, status: :ok
   end
 
   def unassign
-    # TODO: implement the unassign action
+    ReturnDeviceFromUser.new(
+      requesting_user: @current_user,
+      serial_number: params.dig(:device, :serial_number),
+      from_user: params[:owner_id]
+    ).call
+
+    head :ok
+  rescue ReturningError::Unauthorized
+    render json: { error: 'Unauthorized' }, status: :unauthorized
+  rescue UnassigningError::AlreadyUnassigned
+    render json: { error: 'This device is already returned' }, status: :ok
   end
 
   private
